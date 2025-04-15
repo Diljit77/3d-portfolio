22829bf97b146b51de11f3057d9e980d59fffb69
daljit singh <diljit2164@gmail.com>
2025-04-15 20:40:56 +0530

    first commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,33 @@
+
+
+import './App.css'
+import NavBar from './components/NavBar'
+import Contact from './sections/Contact'
+import Experience from './sections/Experience'
+import FeatureCards from './sections/FeatureCards'
+import Footer from './sections/Footer'
+import Hero from './sections/Hero'
+import LogoShowcase from './sections/LogoShowcase'
+import AppShowcase from './sections/ShowcaseSection'
+import TechStack from './sections/TechStack'
+import Testimonials from './sections/Testimonail'
+
+function App() {
+
+  return (
+<>
+<NavBar />
+<Hero />
+<AppShowcase />
+
+<FeatureCards />
+<Experience />
+<TechStack />
+{/* <Testimonials /> */}
+<Contact />
+<Footer />
+</>
+  )
+}
+
+export default App
